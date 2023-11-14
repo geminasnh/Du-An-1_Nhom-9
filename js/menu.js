0701b55a492ac70c2a0ac2c15abfe58b7c0c254f
ngoducduy1999 <147904350+ngoducduy1999@users.noreply.github.com>
2023-11-15 00:14:49 +0700

    updatetrangchu

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,25 @@
-document.getElementById("show").style.display = 'none';
+document.getElementById("show2").style.display = 'none';
+
 document.getElementById("btn1").onclick = function () {
          document.getElementById("show").style.display = 'block';
 
      };
-     document.getElementById("btn2").onclick = function () {
+document.getElementById("btn2").onclick = function () {
         document.getElementById("show").style.display = 'none';
 
     };
+document.getElementById("btn3").onclick = function () {
+        document.getElementById("show2").style.display = 'block';
+        document.getElementById("show1").style.display = 'none';
+
+
+    };
+document.getElementById("btn2-1").onclick = function () {
+       document.getElementById("show2").style.display = 'none';
+       document.getElementById("show1").style.display = 'block';
+
+
+   };
   
 
 //]]>
