6fe107a3d89b665bc80c6ba0a9c433b73178aa78
ngoducduy1999 <147904350+ngoducduy1999@users.noreply.github.com>
2023-11-19 22:05:45 +0700

    updatelai

--- js/menu.js
+++ js/menu.js
@@ -20,6 +20,37 @@ document.getElementById("btn2-1").onclick = function () {
 
 
    };
-  
+   document.getElementById("btn_lisx_down").onclick = function () {
+    document.getElementById("lisx").style.opacity = '1';
+    document.getElementById("lisx").style.visibility = 'visible';
+    document.getElementById("btn_lisx_down").style.display = 'none';
+    document.getElementById("btn_lisx_up").style.display = 'block';
+
+
+};
+document.getElementById("btn_lisx_up").onclick = function () {
+    document.getElementById("lisx").style.opacity = '0';
+    document.getElementById("lisx").style.visibility = 'hidden';
+    document.getElementById("btn_lisx_up").style.display = 'none';
+    document.getElementById("btn_lisx_down").style.display = 'block';
+
+
+
+};
+document.getElementById("loc").style.display = 'none';
+
+document.getElementById("btn_loc").onclick = function () {
+    document.getElementById("main-loc").style.display = 'flex';
+
+    document.getElementById("main-loc").style.alignItems = 'start';
+    document.getElementById("main-loc").style.gap = '1rem';
+    document.getElementById("loc").style.width = '30%';
+document.getElementById("loc").style.display = 'block';
+document.getElementById("loc").style.position = 'sticky';
+document.getElementById("loc").style.top = '35px';
+};
+
+/* scroll menu */
+
 
 //]]>
